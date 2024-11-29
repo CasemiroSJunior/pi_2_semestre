20ddb5232f05592ab6c9c42e1ca62ead59e32f3d
João Carossine <133826422+joaopcarossine@users.noreply.github.com>
2024-11-29 02:45:52 -0300

    Update manipulaUsuario.php

--- web/classes/manipulaUsuario.php
+++ web/classes/manipulaUsuario.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'conexaoBanco.php';
+
 class manipulaUsuario
 {
     private $conexao;
@@ -13,5 +14,52 @@ class manipulaUsuario
     {
         $this->conexao = null;
     }
-}    
-?>    
+
+    public function cadastroUsuario($nome, $usuario, $senha)
+    {
+        
+        $sql = "INSERT INTO usuario (Nome, Usuario, Senha) VALUES (:nome, :usuario, :senha)";
+        $stmt = $this->conexao->conectar()->prepare($sql);
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':usuario', $usuario);
+        $stmt->bindParam(':senha', $senha);
+
+        return $stmt->execute(); 
+    }
+
+    public function listaUsuario()
+    {
+        $sql = "SELECT * FROM usuario";
+        $stmt = $this->conexao->conectar()->query($sql);
+        return $stmt->fetchAll();
+    }
+
+    public function atualizaUsuario($id, $nome, $usuario, $senha)
+    {
+        if ($senha) {
+            $sql = "UPDATE usuario SET Nome = :nome, Usuario = :usuario, Senha = :senha WHERE Id_Usuario = :id";
+            $stmt = $this->conexao->conectar()->prepare($sql);
+            $stmt->bindParam(':senha', $senha);
+        } else {
+            
+            $sql = "UPDATE usuario SET Nome = :nome, Usuario = :usuario WHERE Id_Usuario = :id";
+            $stmt = $this->conexao->conectar()->prepare($sql);
+        }
+
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':usuario', $usuario);
+
+        return $stmt->execute();
+    }
+
+    public function removeUsuario($id)
+    {
+        $sql = "DELETE FROM usuario WHERE Id_Usuario = :id";
+        $stmt = $this->conexao->conectar()->prepare($sql);
+        $stmt->bindParam(':id', $id);
+
+        return $stmt->execute(); 
+    }
+}
+?>
